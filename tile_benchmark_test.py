5a4a5627768234778e5365e34ca6cd8d29d4f7fb
hrodmn <henry.rodman@gmail.com>
2024-12-13 12:14:23 -0600

    fix benchmark script

--- tile_benchmark_test.py
+++ tile_benchmark_test.py
@@ -5,18 +5,25 @@ import httpx
 import pytest
 from morecantile import tms
 
-TILE_URLS = {
-    "titiler-pgstac": "https://titiler-pgstac.maap-project.org/collections/icesat2-boreal/tiles/WebMercatorQuad/{z}/{x}/{y}",
-    "mosaicjson": "https://titiler.maap-project.org/mosaics/cb39d9f9-4b7b-4812-a350-629f9f27fa3a/tiles/{z}/{x}/{y}.png",
+TILE_CONFIGS = {
+    "titiler-pgstac": {
+        "url": "https://titiler-pgstac.maap-project.org/collections/icesat2-boreal/tiles/WebMercatorQuad/{z}/{x}/{y}",
+        "extra_params": {"assets": "tif", "nodata": "nan"},
+    },
+    "mosaicjson": {
+        "url": "https://titiler.maap-project.org/mosaics/cb39d9f9-4b7b-4812-a350-629f9f27fa3a/tiles/{z}/{x}/{y}.png"
+    },
 }
 
 LNG, LAT = -102, 57
+TILES_HEIGHT = 9
+TILES_WIDTH = 7
 
 TMS = tms.get("WebMercatorQuad")
 
 
 def get_surrounding_tiles(
-    x: int, y: int, zoom: int, width: int = 9, height: int = 7
+    x: int, y: int, zoom: int, width: int = TILES_WIDTH, height: int = TILES_HEIGHT
 ) -> List[Tuple[int, int]]:
     """Get a list of surrounding tiles for a viewport"""
     tiles = []
@@ -62,24 +69,52 @@ async def fetch_viewport_tiles(
     warmup_iterations=2,
 )
 @pytest.mark.parametrize("source", ("titiler-pgstac", "mosaicjson"))
-@pytest.mark.parametrize("zoom", (6, 7, 8, 9, 10))
+@pytest.mark.parametrize(
+    "zoom",
+    (
+        6,
+        7,
+        8,
+        9,
+        10,
+    ),
+)
 def test_tiles(benchmark, source: str, zoom: int):
-    tile_url = TILE_URLS.get(source)
+    tile_config = TILE_CONFIGS.get(source)
+    assert tile_config
+
+    tile_url = tile_config["url"]
 
     params = {
         "bidx": 1,
-        "rescale": [0, 400],
+        "rescale": "0,400",
         "colormap_name": "gist_earth_r",
         "color_formula": "gamma r 1.06",
+        **tile_config.get("extra_params", {}),
     }
 
+    error_count = 0
+    success_count = 0
+
     def tile_benchmark():
+        nonlocal error_count, success_count
         assert tile_url
         result = asyncio.run(
             fetch_viewport_tiles(
                 url=tile_url, zoom=zoom, lng=LNG, lat=LAT, params=params
             )
         )
+
+        for res in result:
+            if res.status_code < 500:
+                success_count += 1
+            else:
+                error_count += 1
+
         return result
 
     result = benchmark(tile_benchmark)
+
+    benchmark.extra_info.update(
+        {"error_count": error_count, "success_count": success_count}
+    )
